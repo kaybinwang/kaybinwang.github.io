0edcadbfc992e4cf88025699f44d10f8c3950f47
Kevin Wang <kaybinwang@gmail.com>
2017-05-07 19:47:35 -0400

    resizing of text throught flowtype.js

---
+++ js/resize.js
@@ -0,0 +1,13 @@
+$('body').flowtype({
+}),
+$('.name').flowtype({
+  minFont   : 20,
+  maxFont   : 36,
+  fontRatio : 7
+}),
+$('.nav').flowtype({
+  minFont   : 5,
+  maxFont   : 11,
+  fontRatio : 20
+});
+
